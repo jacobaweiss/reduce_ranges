9c84836b1cf484a77612905e6237d3ae4bddd037
Daryl Yeo <eghhhh@gmail.com>
2014-06-29 02:49:20 -0700

    Added Maximum Sub Array method

--- lib/reduce_ranges.rb
+++ lib/reduce_ranges.rb
@@ -15,4 +15,33 @@ class Array
       arr[index..range_index-1] = (arr[index]..arr[range_index-1]) unless index == range_index - 1
     end
   end
+
+  def max_sub_array
+    raise TypeError unless all? { |el| el.is_a?(Fixnum) }
+
+    max_sum = 0
+    max_start = 0
+    max_end = 0
+    curr_sum = 0
+    curr_start = 0
+    curr_end = 0
+
+    arr = self.dup
+    arr.each_with_index do |el, index|
+      curr_sum += el
+
+      if curr_sum > max_sum
+        max_sum = curr_sum
+        curr_end = index
+        max_start = curr_start
+        max_end = curr_end
+      elsif curr_sum < 0
+        curr_sum = 0
+        curr_start = index + 1
+        curr_end = index + 1
+      end
+    end
+
+    arr.slice(max_start, max_end - max_start + 1)
+  end
 end
